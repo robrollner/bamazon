1075f5d712755220e0ff3ad7f7de336a68c1285a
Robert Rollner <robrollner@gmail.com>
2017-10-09 21:24:49 -0600

    basic framework misssing logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var mysql = require('mysql');
+var fs = require('fs');
 var colors = require('colors');
 var inquirer = require('inquirer');
+// var inventory = require('./inventory.json');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,10 +13,88 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-        if (err) throw err;
-        console.log('connected as ID'.green + connection.threadId);
-    }) //connection works
+    if (err) throw err;
+    console.log('connected as ID'.green + connection.threadId);
+    customerInquiry();
+});
+//connection works
 
+function customerInquiry() {
+    inquirer.prompt([{
+        type: 'list',
+        message: 'Which department is your item located?'.america,
+        choices: ['grocery'.bgCyan.yellow, 'electronics'.bgYellow.cyan, 'clothing'.bgCyan.yellow, 'exit'.bgYellow.cyan],
+        name: 'departments'
+    }]).then((dept) => {
+        switch (dept.departments) {
+            case "grocery":
+                grocery();
+                break;
+
+            case 'electronics':
+                electronics();
+                break;
+
+            case 'clothing':
+                clothing();
+                break;
+
+            case 'exit':
+                exit();
+                break;
+        }
+    })
+}
+
+function grocery() {
+    inquirer.prompt([{
+        name: "food",
+        type: 'list',
+        message: "How can we help you?",
+        choices: ['ice cream', 'wine', 'whiskey', 'amaro']
+    }]).then((answer) => {
+
+        // var items = answer;
+        // var query = `SELECT * FROM products WHERE ?`;
+        // connection.query(query, { food: answer.food }, function(err, res) {
+        //         for (var i = 0; i < res.length; i++) {
+        //             console.log(`see if it works`);
+        //         }
+        //     })
+        //item in ('${items}')
+
+    })
+    logIt();
+}
+
+function electronics() {
+    inquirer.prompt([{
+
+    }])
+
+    logIt();
+}
+
+function clothing() {
+    inquirer.prompt([{
+
+    }])
+    logIt();
+}
+
+function exit() {
+    // if (err) throw err;
+    logIt();
+    console.log("Thank you for shopping with RobCo, please come again".italic.cyan);
+}
 
 
 connection.end(); //end of connection
+
+function logIt(buyerResults) {
+    console.log(buyerResults);
+    var now = new Date();
+    fs.appendFile('inventory.json', "\n" + now + ": " + buyerResults, function(err, data) {
+        if (err) throw err;
+    });
+}
